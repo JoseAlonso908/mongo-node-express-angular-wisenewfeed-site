024527854c16fa6d3e573d43d84eba192ba129f6
Jayaprakash Harikrishnan <prakashjp.h@gmail.com>
2017-08-05 21:26:56 +0530

    Fix: Url filter not processing anchors

--- js/filters.js
+++ js/filters.js
@@ -83,12 +83,13 @@ angular.module('er.filters', [])
         input = decodeHtmlEntity(input)
 
         //URLs starting with http://, https://, or ftp://
-        replacePattern1 = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
+        //replacePattern1 = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
+        replacePattern1 = /((?:(?:ht|f)tps?:\/\/|www)[^<>\]]+?(?![^<>\]]*([>]|<\/))(?=[\s!,?\]]|$))/gmi;
         replacedText = input.replace(replacePattern1, '<a href="$1" target="_blank">$1</a>');
 
         //URLs starting with "www." (without // before it, or it'd re-link the ones done above).
-        replacePattern2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
-        replacedText = replacedText.replace(replacePattern2, '$1<a href="http://$2" target="_blank">$2</a>');
+        //replacePattern2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
+        //replacedText = replacedText.replace(replacePattern2, '$1<a href="http://$2" target="_blank">$2</a>');
 
         //Change email addresses to mailto:: links.
         replacePattern3 = /(([a-zA-Z0-9\-\_\.])+@[a-zA-Z\_]+?(\.[a-zA-Z]{2,6})+)/gim;
